cde8d2cfe25583ebec2810633c60bf9a31b79f6d
Korvin <korvin1986@gmail.com>
2014-04-23 22:09:14 +0300

    Add win/lose points Update jQuery to 2.1.0 Add Reset button
    Fix game logic

--- day2.js
+++ day2.js
@@ -1,11 +1,15 @@
 /**
  * Created by kerkins on 23.04.14.
  */
+
+var READY_STATUS = true;
+
 $(document).ready(function(){
 
-    var userChoice1 = document.getElementsByTagName('img')[0];
-    var userChoice2 = document.getElementsByTagName('img')[1];
-    var userChoice3 = document.getElementsByTagName('img')[2];
+    //var userChoice1 = document.getElementsByTagName('img')[0];
+    //var userChoice2 = document.getElementsByTagName('img')[1];
+    //var userChoice3 = document.getElementsByTagName('img')[2];
+
 
 //    var userChoice = function(){
 //
@@ -38,28 +42,37 @@ $(document).ready(function(){
 //        alert ("loser!");
 //    }
 //};
-    var i=0;
+
 
 $('.click').click(function(){
-    var computer = Math.random(1,0);
-    i++;
+    if(!READY_STATUS) return;
+    var computer = Math.random();
     if(computer <=0.66){
 
         $('#underline').hide();
-        $('#choose').append('<h1>Loser!</h1>');
+        $('#choose').append('<h1 class="status">Loser!</h1>');
 
         $(this).addClass("loser");
+        var lose = $('#loses_amount')[0];
+        lose.innerText = parseInt(lose.innerText) + 1;
+        READY_STATUS = false;
     }else{
         $('#underline').hide();
-        $('#choose').append('<h2>You Win!</h2>');
+        $('#choose').append('<h2 class="status">You Win!</h2>');
         $(this).attr('src', "metal-ball2.jpg");
         $(this).addClass('winner');
-
-
+        var win = $('#wins_amount')[0];
+        win.innerText = parseInt(win.innerText) + 1;
+        READY_STATUS = false;
     }
-    console.log(i);
+});
+
 });
 
 
-
-})
+function reset(){
+    $('#underline').show();
+    $('.click').attr('src', "1275328679_g-b_stern_pearl-at-9-10.jpg").removeClass('winner').removeClass('loser');
+    $('.status').remove();
+    READY_STATUS = true;
+}
