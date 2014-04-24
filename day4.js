1dbb0b5d32df6bb6cce59f2b9c67a63d7c0d8bab
kerkins <brumeregan@gmail.com>
2014-04-24 19:54:03 +0300

    add day4(coloring divs)

---
+++ day4.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kerkins on 24.04.14.
+ */
+$(document).ready(function(){
+var paint;
+$(".color").click(function getColor(){
+  paint =  $(this).css('background-color');
+});
+$('.row').click(function change(){
+    $(this).css('background-color', paint);
+});
+
+})
