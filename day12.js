14e4c289b48ad3fa1e3dc018bd7c840ce8536c29
Korvin <korvin1986@gmail.com>
2014-05-06 22:31:52 +0300

    improve code for game "Dice";

--- day12.js
+++ day12.js
@@ -1,92 +1,42 @@
 /**
  * Created by kerkins on 06.05.14.
  */
- $(document).ready(function(){
-     $('.four').hide();
-
-   var roll1 = $('#first');
-     var roll2 = $('#second');
-
+$(document).ready(function(){
+    $('.four').hide();
     $('#shake').click(function(){
-        getRandomOne();
-        getRandomTwo();
+        getRandom("first");
+        getRandom("second");
     });
 
 
 
-function getRandomOne(){
- var rolling = Math.floor(Math.random() * 6 + 1);
-alert(rolling);
+function getRandom(dice){
+    var rolling = Math.floor(Math.random() * 6 + 1);
 
-var all = $('#first > .one,.two, .three, .four, .five, .six, .seven');
-    if(rolling == 1){
-        $( all).hide();
-        $('#first > .four').show();
+    var all = $('#' + dice + ' > .cube');
+        all.hide();
+        switch (rolling){
+            case 1:
+                $('#' + dice + ' > .four').show();
+                break;
+            case 2:
+                $('#' + dice + ' > .one, #' + dice + ' > .seven').show();
+                break;
+            case 3:
+                $('#' + dice + ' > .one, #' + dice + ' > .four, #' + dice + ' > .seven').show();
+                break;
+            case 4:
+                $('#' + dice + ' > .one, #' + dice + ' > .three, #' + dice + ' > .five, #' + dice + ' > .seven').show();
+                break;
+            case 5:
+                $('#' + dice + ' > .one, #' + dice + ' > .three , #' + dice + ' > .four , #' + dice + ' > .five , #' + dice + ' > .seven').show();
+                break;
+            case 6:
+                all.show();
+                $('#' + dice + ' > .four').hide();
+                break;
+        }
         return rolling;
-    }
-    if (rolling == 2){
-        $(all).hide();
-        $('#first > .one, .seven').show();
-        return rolling;
-    }
-    if(rolling == 3){
-        $(all).hide();
-        $('#first > .one, .four, .seven').show();
-        return rolling;
-    }
-    if(rolling == 4){
-        $(all).hide();
-        $('#first > .one, .three, .five, .seven').show();
-        return rolling;
-    }
-    if(rolling == 5){
-        $(all).hide();
-        $('#first > .one, .three , .four , .five , .seven').show();
-        return rolling;
-    }
-    else{
-        $(all).show();
-        $('#first > .four').hide();
-        return rolling;
-    }
-
 }
 
-     function getRandomTwo(){
-         var rolling2 = Math.floor(Math.random() * 6 + 1);
-alert(rolling2);
-
-         var all = $('#second > .one,.two, .three, .four, .five, .six, .seven');
-         if(rolling2 == 1){
-             $( all).hide();
-             $('#second > .four').show();
-             return rolling2;
-         }
-         if (rolling2 == 2){
-             $(all).hide();
-             $('#second > .one, .seven').show();
-             return rolling2;
-         }
-         if(rolling2 == 3){
-             $(all).hide();
-             $('#second > .one, .four, .seven').show();
-             return rolling2;
-         }
-         if(rolling2 == 4){
-             $(all).hide();
-             $('#second > .one, .three, .five, .seven').show();
-             return rolling2;
-         }
-         if(rolling2 == 5){
-             $(all).hide();
-             $('#second > .one, .three , .four , .five , .seven').show();
-             return rolling2;
-         }
-         else{
-             $(all).show();
-             $('#second > .four').hide();
-             return rolling2;
-         }
-
-     }
- });
+});
