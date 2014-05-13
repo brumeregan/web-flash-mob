b96c7d7257e8cd3808677314321b7f166acb6435
kerkins <brumeregan@gmail.com>
2014-05-13 14:18:57 +0300

    days 13 (not finished) day14 (with questions) day 15 (surplus of adding animation classes)

---
+++ day15.js
@@ -0,0 +1,35 @@
+/**
+ * Created by kerkins on 13.05.14.
+ */
+$(document).ready(function (){
+
+    for(var i = 0; i < 10; i++){
+        $("#main").append("<div class='colors'></div>");
+//        $('.colors').css('background-color', colorBackground[i]);
+    }
+
+    function changingColors(){
+        return '#' + Math.random().toString(16).slice(2, 8);
+    }
+
+    $('.colors').hover(function(){
+            $(this).css('background-color', changingColors());
+     $(this).toggleClass('coloring');
+  },
+        function(){
+              $(this).toggleClass('coloring');
+        });
+
+$('.colors').on('click', function (){
+    var animationsArray = ['crawl', 'dangle', 'jump', 'stretch', 'flip'];
+
+    var animation = animationsArray[Math.floor(Math.random()*5)];
+
+    $(this).addClass(animation);
+
+    setTimeout(function(){
+        $(this).removeClass(animation);
+    }, 1000);
+});
+
+})
